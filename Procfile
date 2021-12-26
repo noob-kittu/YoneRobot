f1427af0d99554920352ad49d3a5a322da891bef
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-12-26 20:35:56 +0530

    New Interface and things are going to publish on this repo

--- Procfile
+++
@@ -1,2 +0,0 @@
-worker: python3 -m YoneRobot
-ps:scale worker=1
--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-TITLE Yone Robot
-:: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m YoneRobot
