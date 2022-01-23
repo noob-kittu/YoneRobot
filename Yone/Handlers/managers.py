ca824ba705910c2dae9c1e9e16353ecc441eb6a6
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-01-24 03:43:06 +0530

    Update managers.py

--- Yone/Handlers/managers.py
+++ Yone/Handlers/managers.py
@@ -19,9 +19,8 @@ else:
 
 class AntiSpam:
     def __init__(self):
-        self.whitelist = (
-            (DEV_USERS or []) + (INSPECTOR or []) + (REQUESTER or [])
-        )
+        self.whitelist = (DEV_USERS or []) + (INSPECTOR or []) + (REQUESTER or [])
+    
         # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
         self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
