a8ecf12d87a59581e43668eac3969754fb80385a
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-01-24 16:07:16 +0530

    Update __main__.py

--- Yone/__main__.py
+++ Yone/__main__.py
@@ -92,7 +92,7 @@ buttons = [
             text="➕️ Add me to your chat ➕️", url="t.me/Yone_Robot?startgroup=true"),
     ],
     [
-        InlineKeyboardButton(text="Admin", callback_data="admin_back"),
+        InlineKeyboardButton(text="Admin", callback_data="admin_"),
         InlineKeyboardButton(
             text="Users", callback_data="users_"
         ),
