eadf3d3faa019b84832620657bd7903c0fd0c32c
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-01-24 14:21:31 +0530

    Update __help__.py

--- Yone/__help__.py
+++ Yone/__help__.py
@@ -46,7 +46,7 @@ for module_name in ALL_MODULES:
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
-    if imported_module.__mod_name__.lower() not in IMPORTED:
+    if imported_module.__mod_name__.lower() in IMPORTED:
         IMPORTED[imported_module.__mod_name__.lower()] = imported_module
     else:
         raise Exception("Can't have two modules with the same name! Please change one")
