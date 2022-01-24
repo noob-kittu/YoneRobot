f7e26eb660f3ee56b1cb58a22a6eb02fe2f53d61
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-01-24 13:44:21 +0530

    Update managers.py

--- Yone/Handlers/managers.py
+++ Yone/Handlers/managers.py
@@ -1,8 +1,12 @@
 import Yone.Database.blacklistusers_sql as sql
 from Yone import ALLOW_EXCL
 from Yone import DEV_USERS, INSPECTOR, REQUESTER
-TIGERS = None
-WOLVES = None
+
+SETS = (
+            (DEV_USERS or [])
+            + (INSPECTOR or [])
+            + (REQUESTER or [])
+        )
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
@@ -22,13 +26,7 @@ else:
 
 class AntiSpam:
     def __init__(self):
-        self.whitelist = (
-            (DEV_USERS or [])
-            + (INSPECTOR or [])
-            + (WOLVES or [])
-            + (REQUESTER or [])
-            + (TIGERS or [])
-        )
+        self.whitelist = SETS
         # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
         self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
