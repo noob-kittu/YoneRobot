1230b3f248ef5016c3c81dd6032436d3aa7062e4
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-01-26 01:46:10 +0530

    Update afk.py

--- Yone/Plugins/User/afk.py
+++ Yone/Plugins/User/afk.py
@@ -18,7 +18,7 @@ AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
 
 
-@run_async
+
 def afk(update: Update, context: CallbackContext):
     args = update.effective_message.text.split(None, 1)
     user = update.effective_user
@@ -48,7 +48,7 @@ def afk(update: Update, context: CallbackContext):
         pass
 
 
-@run_async
+
 def no_longer_afk(update: Update, context: CallbackContext):
     user = update.effective_user
     chat = update.effective_chat
@@ -81,7 +81,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
             return
 
 
-@run_async
+
 def reply_afk(update: Update, context: CallbackContext):
     bot = context.bot
     message = update.effective_message
@@ -162,12 +162,12 @@ def check_afk(update: Update, context: CallbackContext, user_id: int, fst_name: 
             update.effective_message.reply_text(res, parse_mode="html")
 
 
-AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
+AFK_HANDLER = DisableAbleCommandHandler("afk", afk, run_async=True)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"^(?i)brb(.*)$"), afk, friendly="afk",
 )
-NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
-AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk, run_async=True)
+AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk, run_async=True)
 
 dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
