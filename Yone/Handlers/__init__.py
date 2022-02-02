8172767ff5d71e315a73d3d56f53ee7f3c878e3d
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-02-02 21:42:24 +0530

    License

---
+++ Yone/Handlers/__init__.py
@@ -0,0 +1,4 @@
+# BSD 2-Clause License
+
+# Copyright (c) 2022, Kushal
+# All rights reserved.
--- Yone/Plugins/__init__.py
+++ Yone/Plugins/__init__.py
@@ -1,3 +1,8 @@
+# BSD 2-Clause License
+
+# Copyright (c) 2022, Kushal
+# All rights reserved.
+
 from Yone import LOAD, LOGGER, NO_LOAD
 
 
