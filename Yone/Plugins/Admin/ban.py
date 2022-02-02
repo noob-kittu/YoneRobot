f8e210e7896cbdb3fa21b8926980168de57046a6
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2022-02-02 21:54:08 +0700

    bans: deprecated
    since this repo's run on ptb 13.9

--- Yone/Plugins/Admin/ban.py
+++ Yone/Plugins/Admin/ban.py
@@ -105,7 +105,7 @@ def ban(update: Update, context: CallbackContext) -> str:
         log += "\n<b>Reason:</b> {}".format(reason)
 
     try:
-        chat.kick_member(user_id)
+        chat.ban_member(user_id)
 
         if silent:
             if message.reply_to_message:
@@ -240,7 +240,7 @@ def temp_ban(update: Update, context: CallbackContext) -> str:
         log += "\n<b>Reason:</b> {}".format(reason)
 
     try:
-        chat.kick_member(user_id, until_date=bantime)
+        chat.ban_member(user_id, until_date=bantime)
         # bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         bot.sendMessage(
             chat.id,
