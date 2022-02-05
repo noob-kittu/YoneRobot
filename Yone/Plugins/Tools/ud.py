40560ec9d34d527d5308f618a54ff68aec445d70
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-02-06 02:18:04 +0530

    tools

---
+++ Yone/Plugins/Tools/ud.py
@@ -0,0 +1,26 @@
+import requests
+from Yone import dispatcher
+from Yone.Plugins.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+
+
+def ud(update: Update, context: CallbackContext):
+    message = update.effective_message
+    text = message.text[len("/ud ") :]
+    results = requests.get(
+        f"https://api.urbandictionary.com/v0/define?term={text}",
+    ).json()
+    try:
+        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+    except:
+        reply_text = "No results found."
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+
+UD_HANDLER = DisableAbleCommandHandler(["ud"], ud, run_async=True)
+
+dispatcher.add_handler(UD_HANDLER)
+
+__command_list__ = ["ud"]
+__handlers__ = [UD_HANDLER]
