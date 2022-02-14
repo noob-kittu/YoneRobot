7201c585f902bd53bfba1f23e0b9992c772da989
Kittu <70826024+noob-kittu@users.noreply.github.com>
2022-02-14 20:35:58 +0530

    Update muting.py

--- Yone/Plugins/Admin/muting.py
+++ Yone/Plugins/Admin/muting.py
@@ -241,9 +241,9 @@ def temp_mute(update: Update, context: CallbackContext) -> str:
 # Help moved to admins
 __help__ = """
 *Admins only:*
- • `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
- • `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- • `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+  ‣ `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
+  ‣ `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+  ‣ `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
 """
 
 MUTE_HANDLER = CommandHandler("mute", mute, run_async=True)
